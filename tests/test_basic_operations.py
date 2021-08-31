b489b3c87d33b732c774a749702f2a890a7d8f8a
TheusStremens <matheus.beep@gmail.com>
2021-08-31 17:50:20 -0300

    Add magnitude, normalize, and its respective unit tests

--- tests/test_basic_operations.py
+++ tests/test_basic_operations.py
@@ -1,3 +1,5 @@
+import math
+
 import pytest
 
 from udacity.vector import Vector
@@ -32,3 +34,22 @@ def test_constructor_fails():
         Vector()
     with pytest.raises(TypeError):
         Vector(2.8)
+
+
+def test_magnitude():
+    v = Vector([-0.211, 7.437])
+    w = Vector([8.813, -1.331, -6.247])
+    assert math.isclose(v.magnitude(), 7.440, rel_tol=1e-3)
+    assert math.isclose(w.magnitude(), 10.884, rel_tol=1e-3)
+
+
+def test_normalize():
+    v = Vector([5.581, -2.136])
+    w = Vector([1.996, -3.108, -4.554])
+    print(v.normalize())
+    print(w.normalize())
+    assert v.normalize() == Vector([0.934, -0.357])
+    assert w.normalize() == Vector([0.340, -0.530, -0.777])
+    with pytest.raises(ZeroDivisionError):
+        zero_v = Vector([0.0, 0.0, 0.0])
+        zero_v.normalize()
